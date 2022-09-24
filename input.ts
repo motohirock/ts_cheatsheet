f6d6a063e11a9ae011fdf21cd6424e00c7395758
motohirock <m.ece0113@gmail.com>
2022-09-24 14:56:46 +0900

    add split by space

--- input.ts
+++ input.ts
@@ -1,5 +1,10 @@
 import * as fs from "fs"
 {
-const inputs = fs.readFileSync("/dev/stdin", "utf8")
-console.log(inputs)
+    // basicinput
+    // const inputs = fs.readFileSync("/dev/stdin", "utf8")
+    // console.log(inputs)
+    // if neet to split with space
+    
+    const inputAfterSplit = fs.readFileSync("/dev/stdin", "utf-8").split(" ")
+    console.log(inputAfterSplit)
 }
