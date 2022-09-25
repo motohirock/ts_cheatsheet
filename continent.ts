78f5eb36a1a2cea0a7178d7f75f3eb08401b2806
motohirock <m.ece0113@gmail.com>
2022-09-25 18:54:35 +0900

    add classes

---
+++ continent.ts
@@ -0,0 +1,9 @@
+class Continent {
+    name : string;
+    countries : Country[];
+
+    constructor(name: string, countries : Country[]) {
+        this.name = name;
+        this.countries = countries;
+    }
+}
---
+++ country.ts
@@ -0,0 +1,9 @@
+class Country {
+    name : string;
+    capitalCity : string;
+
+    constructor(name : string, capitalCity : string) {
+        this.name = name;
+        this.capitalCity = capitalCity;
+    }
+}
